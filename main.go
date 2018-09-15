678009466332a74bc1b765c81997a198212f71c4
johnstonjacob <jacob@johnstonjacob.com>
2018-09-15 15:15:45 -0500

    main http handler setup

--- main.go
+++ main.go
@@ -1,23 +1,69 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"os"
+)
 
 var (
-  port string
+	port string
 )
 
 func init() {
-  port = os.Getenv("LOCATION_SERVICE_PORT")
+	port = os.Getenv("LOCATION_SERVICE_PORT")
+	if port == "" {
+		port = ":8080"
+	}
 }
 
 func main() {
 	// Hello world, the web server
 
-	helloHandler := func(w http.ResponseWriter, req *http.Request) {
-		io.WriteString(w, "Hello, world!\n")
-	}
-
-	http.HandleFunc("/hello", helloHandler)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	http.HandleFunc("/api/location_service/midpoint", midpointHandler)
+	http.ListenAndServe(":8080", nil)
 }
 
+type midpointBody struct {
+	Cities []string `json:"cities"`
+}
+
+type midpointRes struct {
+	Lat  int `json:"lat"`
+	Long int `json:"lon"`
+}
+
+func midpointHandler(w http.ResponseWriter, req *http.Request) {
+
+	body, err := parseJson(req.Body)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	_ = body
+	res, err := json.Marshal(midpointRes{24, 25})
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(res)
+}
+
+func parseJson(body io.ReadCloser) (midpointBody, error) {
+	var request midpointBody
+	buf, err := ioutil.ReadAll(body)
+	defer body.Close()
+
+	if err != nil {
+		return midpointBody{}, err
+	}
+
+	err = json.Unmarshal(buf, &request)
+	if err != nil {
+		return midpointBody{}, err
+	}
+
+	return request, nil
+}
