f759990418b9d2ea3609fc8bc3a38cdabc090505
johnstonjacob <jacob@johnstonjacob.com>
2018-09-15 19:10:52 +0000

    http init

--- main.go
+++ main.go
@@ -2,6 +2,22 @@ package main
 
 import "fmt"
 
-func main() {
-	fmt.Println("vim-go")
+var (
+  port string
+)
+
+func init() {
+  port = os.Getenv("LOCATION_SERVICE_PORT")
 }
+
+func main() {
+	// Hello world, the web server
+
+	helloHandler := func(w http.ResponseWriter, req *http.Request) {
+		io.WriteString(w, "Hello, world!\n")
+	}
+
+	http.HandleFunc("/hello", helloHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
