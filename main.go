09d0441fee019f9dcdcd3fb9eb1d65f89cb28eb6
johnstonjacob <jacob@johnstonjacob.com>
2018-09-16 12:37:55 -0500

    test

--- main.go
+++ main.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
@@ -13,8 +17,10 @@ type midpointBody struct {
 }
 
 type midpointRes struct {
-	StatusCode int  `json:"statusCode"`
-	Body       body `json:"body"`
+	Headers         map[string]string `json:"headers"`
+	IsBase64Encoded bool              `json:"isBase64Encoded"`
+	StatusCode      int               `json:"statusCode"`
+	Body            body              `json:"body"`
 }
 
 type body struct {
@@ -29,16 +35,27 @@ type errorRes struct {
 	Error string `json:"error"`
 }
 
-func midpointHandler(req midpointBody) (midpointRes, error) {
+func midpointHandler(req midpointBody) (events.APIGatewayProxyResponse, error) {
+	headers := make(map[string]string)
+
 	cities := getLatAndLong(req.Cities)
 
 	lat, lng := getLatLngCenter(cities)
+	headers["Location"] = fmt.Sprintf("%f, %f", lat, lng)
 
 	state := findState(lat, lng)
 
 	parks := getParks(state)
 
-	res := midpointRes{200, body{lat, lng, state, parks, cities}}
+	Body := body{lat, lng, state, parks, cities}
+
+	bodyJson, err := json.Marshal(Body)
+	if err != nil {
+		fmt.Println(err)
+		return events.APIGatewayProxyResponse{}, err
+	}
+
+	res := events.APIGatewayProxyResponse{Headers: headers, IsBase64Encoded: false, StatusCode: 200, Body: string(bodyJson)}
 
 	return res, nil
 }
