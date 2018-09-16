cfcc0ef389b1d1538e744a0de6fb8ef06dd9ea6b
johnstonjacob <jacob@johnstonjacob.com>
2018-09-15 22:49:14 -0500

    images

--- parks.go
+++ parks.go
@@ -8,7 +8,7 @@ import (
 
 func getParks(state string) string {
 
-	url := fmt.Sprintf("https://api.nps.gov/api/v1/parks?stateCode=%s&limit=50&q=national%spark", state, "%20")
+	url := fmt.Sprintf("https://api.nps.gov/api/v1/parks?stateCode=%s&limit=50&q=national%spark&fields=images,", state, "%20")
 
 	req, err := http.Get(url)
 	if err != nil {
